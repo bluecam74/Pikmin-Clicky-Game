b0547c28e5c5145a9be1bf2615c3337ddc4d9ec5
bluecam74 <mabdulha@uci.edu>
2019-04-30 19:47:00 -0700

    github 1

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const routes = require("./routes");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 // Import routes
 app.use(routes);
 // Send every other request to the React app
