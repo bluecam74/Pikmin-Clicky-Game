1cc304f32fb77be0f325bd9326af8d6ab7da6524
bluecam74 <mabdulha@uci.edu>
2019-05-18 12:48:43 -0700

    score fixes 2

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,10 @@ import Wrapper from "./components/Wrapper";
 import Score from "./components/Score";
 import cards from "./cards.json";
 
-const EMPTY = {
-  clickedCards: [],
-};
+const EMPTY = [];
 
 class App extends Component {
 
-  
   state = {
     cards,
     clickedCards: EMPTY,
@@ -21,23 +18,20 @@ class App extends Component {
   resetGame = () => {
     this.setState({score: 0});
     this.setState({message: "Don't Click the Same Picture Twice!"});
-    this.setState({ ...EMPTY})
+    this.setState({clickedCards: EMPTY})
   }
 
   checkIfClicked = (id) => {
     if (this.state.clickedCards.includes(id)) {
       this.setState({score: 0});
-      this.setState({clickedCards: []})
       this.setState({message: "You Lost! Try Again"})
-      this.setState({clickedCards: []})
-      console.log("You Lost");
-      console.log("score: ",this.state.score);
+      this.setState({clickedCards: EMPTY})
     } else {
-      console.log("not in array");
       this.setState({ score: this.state.score + 1 })
-      if (this.state.score === 5) {
-        console.log("winner");
-        this.setState({message: "Congratulations! You Won!"})
+      if (this.state.score === 14) {
+        this.setState({message: "Congratulations! You Won!"});
+        this.setState({score: 0});
+        this.setState({clickedCards: EMPTY});
       };
     }
   }
@@ -47,14 +41,11 @@ class App extends Component {
     cards.sort(function(a, b){return 0.5 - Math.random()});
     this.setState({ cards });
     this.setState({clickedCards: this.state.clickedCards.concat(id)});
+    this.setState({message: "Don't Click the Same Picture Twice!"});
     const clicked = this.state.clickedCards
-    console.log("Already Clicked:", clicked);
     this.checkIfClicked(id);
 
   };
-
-  
-
 
   render() {
     return (
